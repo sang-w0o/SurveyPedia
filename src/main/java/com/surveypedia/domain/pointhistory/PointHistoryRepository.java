80417e76486c8ffa5c283606babc2abc31fb1f49
Sangwoo Ra <robbyra@gmail.com>
2020-10-07 13:10:13 +0900

    Survey Buy and sell complete

--- src/main/java/com/surveypedia/domain/pointhistory/PointHistoryRepository.java
+++ src/main/java/com/surveypedia/domain/pointhistory/PointHistoryRepository.java
@@ -2,12 +2,11 @@ package com.surveypedia.domain.pointhistory;
 
 import com.surveypedia.tools.SQL;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
-public interface PointHistoryRepository extends JpaRepository<PointHistory, Integer>, JpaSpecificationExecutor<PointHistory> {
+public interface PointHistoryRepository extends JpaRepository<PointHistory, Integer> {
 
     List<PointHistory> findByEmail(String email);
 
