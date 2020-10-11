e552ab8ffea0e958e90378f9c3c0e4e8503b83ea
Sangwoo Ra <robbyra@gmail.com>
2020-10-11 22:35:36 +0900

    Fix Query.

--- src/main/java/com/surveypedia/tools/SQL.java
+++ src/main/java/com/surveypedia/tools/SQL.java
@@ -217,6 +217,6 @@ public class SQL {
 
     public static class Reports {
         public static final String REMOVE_REPORT_BY_S_CODE = "DELETE FROM reports WHERE s_code=?";
-        public static final String GET_ALL_REPORTED_S_CODE = "SELECT s_code FROM reports WHERE r_state='Y";
+        public static final String GET_ALL_REPORTED_S_CODE = "SELECT s_code FROM reports WHERE r_state='Y'";
     }
 }
